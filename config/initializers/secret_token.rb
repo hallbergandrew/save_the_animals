cfc5f8c44dbbfb6ea9e6eae7e28c7ec19c1ae33b
anne jhetson & Andrew Hallberg <student@epicodus.com>
2014-04-04 09:49:45 -0700

    Initial rails app, needs you to replace database names, has foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaveAnimals::Application.config.secret_key_base = '9ff7a675334de601cf5d48dc12022d43649285a7bdd4c9a8b91fc77bbd66d90bb1bb569687ffc7e4a795ff3e5600c034da1320d6e266d4659c60a07844ac2dea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaveAnimals::Application.config.session_store :cookie_store, key: '_save_animals_session'
